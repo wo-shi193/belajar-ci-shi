aa9ce05694a6ea527ec2ff8a9a92b3e440985c07
wo-shi193 <shintamus19@gmail.com>
2025-06-13 13:19:46 +0700

    first commit

---
+++ app/Views/components/sidebar.php
@@ -0,0 +1,34 @@
+<!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link <?php echo (uri_string() == '') ? "" : "collapsed" ?>" href="/">
+          <i class="bi bi-grid"></i>
+          <span>Home</span>
+        </a>
+      </li><!-- End Home Nav -->
+
+
+      <li class="nav-item">
+        <a class="nav-link <?php echo (uri_string() == '') ? "" : "collapsed" ?>" href="keranjang">
+          <i class="bi bi-cart-check"></i>
+          <span>Keranjang</span>
+        </a>
+      </li><!-- End Keranjang Nav -->
+      <?php
+        if (session()->get('role') == 'admin') {
+        ?>
+          <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == '') ? "" : "collapsed" ?>" href="produk">
+              <i class="bi bi-receipt"></i>
+              <span>Produk</span>
+            </a>
+          </li><!-- End Produk Nav -->
+        <?php
+        }
+        ?>
+    </ul>
+
+  </aside><!-- End Sidebar-->
